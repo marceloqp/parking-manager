c50f485955d01735b11ba17dcacab9fe792c9f6e
Marcelo Pinheiro <quadrelli.marcelo@gmail.com>
2025-05-28 10:55:25 -0300

    feat: commit inicial - Criado estrutura básica do projeto;

---
+++ src/test/java/br/com/mpsolucoes/manager/ParkingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mpsolucoes.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
